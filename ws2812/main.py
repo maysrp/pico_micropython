782f19dcd55b06130c95048cf52572f0f7012891
maysrp <maysrp@gmail.com>
2021-08-08 22:34:48 +0800

    Update main.py

--- ws2812/main.py
+++ ws2812/main.py
@@ -1,8 +1,7 @@
 from neopixel import Neopixel
+pixels = Neopixel(1, 0, 0, "GRB")
 
-pixels = Neopixel(10, 0, 0, "GRB")
-
-# 10个灯 使用GPIO0 状态0 使用RGB（GRB）
+# 10个灯 状态 使用GPIO0  使用RGB（GRB）
 # 从0开始
 
 pixels.set_pixel(5, (10, 0, 0))
